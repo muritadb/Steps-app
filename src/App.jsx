038d2b87b1caff951aa8cca683e53ee3ec695be8
Douglas Murita <douglasb008@gmail.com>
2023-12-06 21:47:45 -0300

    button state for open/close component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,50 @@
+import { useState } from "react"
+
+const steps = [
+  {
+    id: crypto.randomUUID(),
+    description: "Entender o problema do cliente",
+  },
+  {
+    id: crypto.randomUUID(),
+    description: "Desenvolver a solução do problema",
+  },
+  {
+    id: crypto.randomUUID(),
+    description:
+      "Repetir até o cliente ficar feliz e encher seu 🍑 de dinheiro",
+  },
+]
+
 const App = () => {
-  const handleClickToggle = (e) => console.log(e.target.textContent)
+  // const [message, setMessage] = useState(steps[0])
+  const [isOpen, setIsOpen] = useState(true)
+
+  const handleClickToggle = () => setIsOpen((i) => !i)
   const handleClickPrevious = (e) => console.log(e.target.textContent)
-  const handleClickNext = (e) => console.log(e.target.textContent)
+  const handleClickNext = () => {}
 
   return (
     <div>
       <div className="container-close">
         <button onClick={handleClickToggle} className="close">
-          Fechar
+          {isOpen ? "Fechar" : "Abrir"}
         </button>
       </div>
-      <div className="steps">
-        <div className="numbers">
-          <div className="active">1</div>
-          <div>2</div>
-          <div>3</div>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className="active">1</div>
+            <div>2</div>
+            <div>3</div>
+          </div>
+          <p className="message">Entender o problema do cliente</p>
+          <div className="buttons">
+            <button onClick={handleClickPrevious}>Anterior</button>
+            <button onClick={handleClickNext}>Proximo</button>
+          </div>
         </div>
-        <p className="message">Passo 1: Entender o problema do cliente</p>
-        <div className="buttons">
-          <button onClick={handleClickPrevious}>Anterior</button>
-          <button onClick={handleClickNext}>Proximo</button>
-        </div>
-      </div>
+      )}
     </div>
   )
 }
